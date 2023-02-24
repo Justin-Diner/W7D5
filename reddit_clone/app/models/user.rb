1b1952ef7afbe8f003d887ad2d43286774c0e05b
Amir Lamrabat <secretsofthecode9@gmail.com>
2023-02-24 11:38:50 -0500

    routes

--- reddit_clone/app/models/user.rb
+++ reddit_clone/app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-	validates :username, presence: true, uniqueness: true 
+	validates :username,:session_token, presence: true, uniqueness: true 
 	validates :password_digest, presence: true, uniqueness: true 
-	validates :password, length: {minimum: 6}, allow_nil: true 
+	validates :password, length: {minimum: 6}, allow_nil: true
+	 
 
 	before_validation :ensure_session_token 
+
+	attr_reader :password
 
 	def self.find_by_credentials(username, password)
 		user = User.find_by(username: username)
@@ -26,4 +29,25 @@ class User < ApplicationRecord
 		end
 		token
 	end
+
+	def password=(password)
+		@password = password 
+		self.password_digest = BCrypt::Password.create(password)
+
+	end
+
+	def ensure_session_token
+		self.session_token||= generate_session_token
+
+	end
+
+	def reset_session_token
+		self.session_token = generate_session_token
+		self.save!
+		self.session_token
+		
+	end
+
+
+
 end
